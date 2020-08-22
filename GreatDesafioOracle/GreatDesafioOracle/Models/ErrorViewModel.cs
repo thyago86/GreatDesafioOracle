a77d2653ab30a87c718b02e419473a83008b8332
Thyago Bezerra Lima <thyagobezerra8608@gmail.com>
2020-08-22 13:58:56 -0300

    Projeto criado

---
+++ GreatDesafioOracle/GreatDesafioOracle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreatDesafioOracle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
